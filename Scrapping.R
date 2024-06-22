a456d8fb7d3deb0f28f3c7a56ca242cc30975882
Yunna Mentari Indah <158244021+yunnamentari@users.noreply.github.com>
2024-06-22 22:40:04 +0700

    Update Scrapping.R

--- Scrapping.R
+++ Scrapping.R
@@ -9,17 +9,22 @@ message('Loading Packages')
 
 message('Scraping Data')
 
+# Base URL
 base_url <- "https://sinta.kemdikbud.go.id/authors?page="
 
+# Construct URL for the current page
 url <- paste0(base_url, current_page)
 
+# Read HTML of the page
 sinta <- read_html(url)
 
+# Scrape data
 NAMA <- sinta %>% html_nodes(".profile-name") %>% html_text(trim = TRUE)
 SINTA_ID <- sinta %>% html_nodes("div.profile-id") %>% html_text(trim = TRUE) %>% gsub("SINTA ID : ", "", .)
 DEPT <- sinta %>% html_nodes("div.profile-dept") %>% html_text(trim = TRUE) %>% gsub("DEPT : ", "", .)
 INDEKS <- sinta %>% html_nodes("div.pr-txt-metric") %>% html_text(trim = TRUE) %>% gsub("DEPT : ", "", .)
 
+# Create dataframe for the current page
 data <- data.frame(
   time_scraped = Sys.time(),
   NAMA = NAMA,
@@ -28,7 +33,9 @@ data <- data.frame(
   stringsAsFactors = FALSE
 )
 
+# Connect to MongoDB
 connection_string <- Sys.getenv("ATLAS_URL")
 db <- mongo(collection = Sys.getenv("ATLAS_COLLECTION"), db = Sys.getenv("ATLAS_DB"), url = connection_string)
 
+# Insert data into MongoDB
 db$insert(data)
