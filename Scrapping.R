74d2c0b8b6d7643ab41ca8124f9d77f73e8ff7cc
Yunna Mentari Indah <158244021+yunnamentari@users.noreply.github.com>
2024-06-22 22:32:09 +0700

    Scrapping.R

--- Scrapping.R
+++ Scrapping.R
@@ -2,54 +2,33 @@ library(rvest)
 library(tidyverse)
 library(mongolite)
 
-# Inisialisasi URL dasar
+args <- commandArgs(trailingOnly = TRUE)
+current_page <- as.integer(args[1])
+
+message('Loading Packages')
+
+message('Scraping Data')
+
 base_url <- "https://sinta.kemdikbud.go.id/authors?page="
 
-# Fungsi untuk scraping data dari satu halaman
-scrape_page <- function(url) {
-  sinta <- read_html(url)
-  
-  NAMA <- sinta %>% html_nodes(".profile-name") %>% html_text(trim = TRUE)
-  SINTA_ID <- sinta %>% html_nodes("div.profile-id") %>% html_text(trim = TRUE) %>% gsub("SINTA ID : ", "", .)
-  DEPT <- sinta %>% html_nodes("div.profile-dept") %>% html_text(trim = TRUE) %>% gsub("DEPT : ", "", .)
-  UNIV <- sinta %>% html_nodes("div.profile-affil") %>% html_text(trim = TRUE) %>% gsub("UNIV : ", "", .)                               # Ubah ke numerik jika diperlukan
-  
-  data <- data.frame(
-    time_scraped = Sys.time(),
-    NAMA = NAMA,
-    SINTA_ID = SINTA_ID,
-    DEPT = DEPT,
-    UNIV = UNIV,
-    stringsAsFactors = FALSE
-  )
-  
-  return(data)
-}
+url <- paste0(base_url, current_page)
 
-# Inisialisasi data frame untuk menampung semua data
-all_data <- data.frame()
+sinta <- read_html(url)
 
-# Inisialisasi halaman awal dan batas maksimum halaman
-page_number <- 1
-max_pages <- 5
+NAMA <- sinta %>% html_nodes(".profile-name") %>% html_text(trim = TRUE)
+SINTA_ID <- sinta %>% html_nodes("div.profile-id") %>% html_text(trim = TRUE) %>% gsub("SINTA ID : ", "", .)
+DEPT <- sinta %>% html_nodes("div.profile-dept") %>% html_text(trim = TRUE) %>% gsub("DEPT : ", "", .)
+INDEKS <- sinta %>% html_nodes("div.pr-txt-metric") %>% html_text(trim = TRUE) %>% gsub("DEPT : ", "", .)
 
-# Loop untuk scraping dari beberapa halaman
-while(page_number <= max_pages) {
-  # Buat URL untuk halaman saat ini
-  url <- paste0(base_url, page_number)
-  
-  # Scrape data dari halaman saat ini
-  page_data <- scrape_page(url)
-  
-  # Jika data yang didapat kosong, hentikan loop
-  if (nrow(page_data) == 0) {
-    break
-  }
-  
-  # Gabungkan data dari halaman ini ke data sebelumnya
-  all_data <- bind_rows(all_data, page_data)
-  
-  # Update nomor halaman
-  page_number <- page_number + 1
-}
+data <- data.frame(
+  time_scraped = Sys.time(),
+  NAMA = NAMA,
+  SINTA_ID = SINTA_ID,
+  DEPT = DEPT,
+  stringsAsFactors = FALSE
+)
 
+connection_string <- Sys.getenv("ATLAS_URL")
+db <- mongo(collection = Sys.getenv("ATLAS_COLLECTION"), db = Sys.getenv("ATLAS_DB"), url = connection_string)
+
+db$insert(data)
